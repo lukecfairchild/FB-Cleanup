d7ccbbb549d7acdcb7008669f00f6925413cfa63
Luke Fairchild <lukecfairchild@gmail.com>
2015-10-20 23:18:46 -0600

    Refactor: Make more dynamic

--- index.js
+++ index.js
@@ -1,39 +1,78 @@
 
+var parents = [];
+
+var findParents = function ( target ) {
+
+	if ( !target ) {
+		target = document.getElementsByTagName( 'textarea' )[ 0 ];
+	}
+
+	if ( target.parentNode ) {
+		parents.push( target );
+		findParents( target.parentNode );
+	}
+};
+
 var removeSuggestedPosts = function () {
 
-	var spanTags = document.getElementsByTagName( 'span' );
+	var spanTags   = document.getElementsByTagName( 'span' );
 	var searchText = 'Suggested Post';
+	var loopBack   = function ( target ) {
+
+		if ( target.parentNode
+			&& target.parentNode.parentNode
+			&& target.parentNode.parentNode.parentNode
+			&& parents.indexOf( target.parentNode.parentNode.parentNode ) > -1
+		) {
+
+			if ( target.style.display !== 'none' ) {
+				console.log( 'Removed sponsored post.' );
+				target.style.display = 'none';
+			}
+
+		} else if ( target.parentNode ) {
+			loopBack( target.parentNode );
+		}
+	};
 
 	for ( var i = 0; i < spanTags.length; i++ ) {
 
 		if ( spanTags[ i ].textContent == searchText ) {
-			var target = spanTags[ i ].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-
-			if ( target.style.display !== 'none' ) {
-				console.log( 'Removed suggested post' );
-				target.style.display = 'none';
-			}
+			loopBack( spanTags[ i ] );
 		}
 	}
 };
 
 var removeSponsoredPosts = function () {
 
-	var spanTags = document.getElementsByTagName( 'a' );
+	var spanTags   = document.getElementsByTagName( 'a' );
 	var searchText = 'Sponsored';
+	var loopBack   = function ( target ) {
 
-	for ( var i = 0; i < spanTags.length; i++ ) {
-
-		if ( spanTags[ i ].textContent == searchText ) {
-			var target = spanTags[ i ].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+		if ( target.parentNode
+			&& target.parentNode.parentNode
+			&& target.parentNode.parentNode.parentNode
+			&& parents.indexOf( target.parentNode.parentNode.parentNode ) > -1
+		) {
 
 			if ( target.style.display !== 'none' ) {
 				console.log( 'Removed sponsored post.' );
 				target.style.display = 'none';
 			}
+		} else if ( target.parentNode ) {
+			loopBack( target.parentNode );
+		}
+	};
+
+	for ( var i = 0; i < spanTags.length; i++ ) {
+
+		if ( spanTags[ i ].textContent == searchText ) {
+			loopBack( spanTags[ i ] );
 		}
 	}
 };
+
+findParents();
 
 window.addEventListener( 'scroll', removeSuggestedPosts );
 window.addEventListener( 'scroll', removeSponsoredPosts );
