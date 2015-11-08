21d9a6c94d1ee17022a4dd88726aa272a3e0a110
Jared Null <jnull@whitehat.io>
2015-11-07 20:41:55 -0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -5,327 +5,339 @@
 // @version		1.1.1
 // @grant		none
 // ==/UserScript==
-var queue     = {};
-var queueLast = {};;
-var queueTimeout;
+(function FB_CleanUP () {
+		var FB				=  {}
+		,queue				=  {}
+		,queueLast			=  {}
+		,queueTimeout			=  {}
+		,options = {
+			'suggestedPosts'      : false,
+			'sponsoredPosts'      : false,
+			'likedPosts'          : false,
+			'commentedOnPosts'    : false,
+			'repliedToPosts'      : false,
+			'wasTaggedInPosts'    : false,
+			'sharedPosts'         : true,
+			'viaPosts'            : true,
+			'wasMentionedInPosts' : false,
+			'wroteOnPosts'        : false,
+			'appSuggestions'      : false,
+			'trending'            : false,
+			'suggestedPages'      : false
+		};
 
-var options = {
-	'suggestedPosts'      : false,
-	'sponsoredPosts'      : false,
-	'likedPosts'          : false,
-	'commentedOnPosts'    : false,
-	'repliedToPosts'      : false,
-	'wasTaggedInPosts'    : false,
-	'sharedPosts'         : true,
-	'viaPosts'            : true,
-	'wasMentionedInPosts' : false,
-	'wroteOnPosts'        : false,
-	'appSuggestions'      : false,
-	'trending'            : false,
-	'suggestedPages'      : false
-};
+	FB.suggestedPosts = function () {
 
-var FB = {};
+		this.events = ['change'];
+		this.run = function () {
 
-FB.suggestedPosts = function () {
+			filterElements('span', function (element) {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
-
-		filterElements( 'span', function ( element ) {
-
-			if ( element.textContent == 'Suggested Post' ) {
-				removePost( element )
-			}
-		} );
-	}
-};
-
-FB.sponsoredPosts = function () {
-
-	this.events = [ 'change' ];
-	this.run    = function () {
-
-		filterElements( 'a', function ( element ) {
-
-			if ( element.textContent == 'Sponsored' ) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent == 'Suggested Post') {
+					removePost(element)
+				}
+			});
+		}
 	};
-};
 
-FB.likedPosts = function () {
+	FB.sponsoredPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('a', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( 'liked this' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
-	}
-};
-
-FB.commentedOnPosts = function () {
-
-	this.events = [ 'change' ];
-	this.run    = function () {
-
-		filterElements( 'span', function ( element ) {
-
-			if ( element.textContent
-				&& element.textContent.indexOf( 'commented on' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent == 'Sponsored') {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.repliedToPosts = function () {
+	FB.likedPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('span', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( 'replied to' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf('liked this') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		}
 	};
-};
 
-FB.taggedInPosts = function () {
+	FB.commentedOnPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('span', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( ' tagged in ' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf('commented on') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.sharedPosts = function () {
+	FB.repliedToPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('span', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( ' shared ' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf('replied to') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.viaPosts = function () {
+	FB.taggedInPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('span', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( ' via ' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf(' tagged in ') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.wasMentionedInPosts = function () {
+	FB.sharedPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('span', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( ' was mentioned in ' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf(' shared ') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.wroteOnPosts = function () {
+	FB.viaPosts = function () {
 
-	this.events = [ 'change' ];
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		filterElements( 'span', function ( element ) {
+			filterElements('span', function (element) {
 
-			if ( element.textContent
-				&& element.textContent.indexOf( ' wrote on ' ) > -1
-				&& getComputedStyle( element ).color.toString() == 'rgb(145, 151, 163)'
-			) {
-				removePost( element );
-			}
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf(' via ') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.appSuggestions = function () {
+	FB.wasMentionedInPosts = function () {
 
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		waitFor( '#pagelet_canvas_nav_content', function ( element ) {
-			element.remove();
-			document.getElementsByClassName( 'fbChatSidebarBody' )[ 0 ].style.height = '100%';
-		} );
-	}
-};
+			filterElements('span', function (element) {
 
-FB.trending = function () {
-
-	this.run    = function () {
-
-		waitFor( '#pagelet_trending_tags_and_topics', function ( element ) {
-
-			element.remove();
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf(' was mentioned in ') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
-FB.suggestedPages = function () {
+	FB.wroteOnPosts = function () {
 
-	this.run    = function () {
+		this.events = ['change'];
+		this.run = function () {
 
-		waitFor( '#pagelet_ego_pane', function ( element ) {
+			filterElements('span', function (element) {
 
-			element.remove();
-		} );
+				if (element.textContent
+					&& element.textContent.indexOf(' wrote on ') > -1
+					&& getComputedStyle(element).color.toString() == 'rgb(145, 151, 163)'
+				) {
+					removePost(element);
+				}
+			});
+		};
 	};
-};
 
+	FB.appSuggestions = function () {
+
+		this.run = function () {
+
+			waitFor('#pagelet_canvas_nav_content', function (element) {
+				element.remove();
+				document.getElementsByClassName('fbChatSidebarBody')[0].style.height = '100%';
+			});
+		}
+	};
+
+	FB.trending = function () {
+
+		this.run = function () {
+
+			waitFor('#pagelet_trending_tags_and_topics', function (element) {
+
+				element.remove();
+			});
+		};
+	};
+
+	FB.suggestedPages = function () {
+
+		this.run = function () {
+
+			waitFor('#pagelet_ego_pane', function (element) {
+
+				element.remove();
+			});
+		};
+	};
 
 // Library
-var waitFor = function ( locator, callBack ) {
+	var waitFor = function (locator, callBack) {
 
-	if ( typeof locator === 'string' ) {
-		var check = document.querySelector( locator );
+		if (typeof locator === 'string') {
+			var check = document.querySelector(locator);
 
-		if ( check ) {
-			callBack( check );
+			if (check) {
+				callBack(check);
 
-		} else {
-			setTimeout( function () {
+			} else {
+				setTimeout(function () {
 
-				waitFor( locator, callBack );
-			}, 1 );
+					waitFor(locator, callBack);
+				}, 1);
+			}
 		}
-	}
-};
+	};
 
-var removePost = function ( target ) {
+	var removePost = function (target) {
 
-	if ( target.hasAttribute( 'data-cursor' ) ) {
+		if (target.hasAttribute('data-cursor')) {
 
-		if ( target.style.display !== 'none' ) {
-			target.style.display = 'none';
-		}
-
-	} else if ( target.parentNode ) {
-		removePost( target.parentNode );
-	}
-};
-
-var filterElements = function ( element, condition ) {
-
-	if ( !queue[ element ] ) {
-		queue[ element ] = [];
-	}
-
-	queue[ element ].push( condition );
-
-	clearTimeout( queueTimeout );
-
-	queueTimeout = setTimeout( function () {
-
-		for ( var type in queue ) {
-			var last;
-			var contains;
-
-			var tags = document.getElementsByTagName( type );
-
-			if ( queueLast[ type ] ) {
-				contains = tags.indexOf( queueLast[ type ] );
+			if (target.style.display !== 'none') {
+				target.style.display = 'none';
 			}
 
-			for ( var func in queue[ type ] ) {
-
-				for ( var i = ( ( contains > 0 ) ? contains : 0 ); i < tags.length; i++ ) {
-					queue[ type ][ func ]( tags[ i ] );
-				}
-				
-				last = tags[ i ];
-			}
-
-			queueLast[ type ] = last;
+		} else if (target.parentNode) {
+			removePost(target.parentNode);
 		}
-	}, 1 );
-};
+	};
 
-if ( window.top === window.self ) {
-	
-	var active = [];
-	var observer = new MutationObserver( function ( mutations ) {
+	var filterElements = function (element, condition) {
 
-		for ( var i in active ) {
-			active[ i ]();
+		if (!queue[element]) {
+			queue[element] = [];
 		}
-	} );
 
-	observer.observe( document.body, {
-		'attributes'    : true, 
-		'childList'     : true, 
-		'characterData' : true 
-	} );
+		queue[element].push(condition);
 
-	for ( var func in FB ) {
+		clearTimeout(queueTimeout);
 
-		if ( !options[ func ] ) {
-			( function ( func ) {
+		queueTimeout = setTimeout(function () {
 
-				func.run();
+			for (var type in queue) {
+				if (queue.hasOwnProperty(type)) {
+					var last;
+					var contains;
 
-				if ( func.events ) {
+					var tags = document.getElementsByTagName(type);
 
-					for ( var i in func.events ) {
-
-						if ( func.events[ i ] == 'change' ) {
-							active.push( func.run );
-						}
-				//		window.addEventListener( func.events[ i ], func.run );
+					if (queueLast[type]) {
+						contains = tags.indexOf(queueLast[type]);
 					}
+
+					for (var func in queue[type]) {
+
+						if (queue[type].hasOwnProperty(func)) {
+							for (var i = ( ( contains > 0 ) ? contains : 0 ); i < tags.length; i++) {
+
+								queue[type][func](tags[i]);
+
+							}
+
+							last = tags[i];
+						}
+
+					}
+
+					queueLast[type] = last;
 				}
-			} )( new FB[ func ]() );
+			}
+		}, 1);
+	};
+
+	if (window.top === window.self) {
+
+		var active = [];
+		var observer = new MutationObserver(function (mutations) {
+
+			for (var i in active) {
+				if (active.hasOwnProperty(i)) {
+					active[i]();
+				}
+			}
+		});
+
+		observer.observe(document.body, {
+			'attributes': true,
+			'childList': true,
+			'characterData': true
+		});
+
+		for (var func in FB) {
+
+			if (FB.hasOwnProperty(func)) {
+				if (!options[func]) {
+					(function (func) {
+
+						func.run();
+
+						if (func.events) {
+
+							for (var i in func.events) {
+
+								if (func.events.hasOwnProperty(i)) {
+									if (func.events[i] == 'change') {
+										active.push(func.run);
+									}
+									//		window.addEventListener( func.events[ i ], func.run );
+								}
+							}
+						}
+					})(new FB[func]());
+				}
+			}
 		}
 	}
-}
+}());
